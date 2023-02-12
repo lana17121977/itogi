2f736abd5c2451c0f6cc39c41f566666602d2960
Svetlana <121864210+lana17121977@users.noreply.github.com>
2023-02-12 21:03:15 +0300

    проверка длины элементов

--- Irabota/Program.cs
+++ Irabota/Program.cs
@@ -11,3 +11,22 @@
 string[] arr1 = {"Hello", "2", "world", ":-)"};
 string[] arr2 = {"1234", "1567", "-2", "computer science"};
 string[] arr3 = {"Russia", "Denmark", "Kazan"};
+
+string[] GetShortWordsArray(string[] array)
+{
+	int length = array.Length;			
+	string[] result = new string[length];
+	int count = 0;
+			
+	for (int i = 0; i < length; i++)
+	{
+		if(array[i].Length <= MAX_WORD_LENGTH)
+		{
+			result[count] = array[i];
+			count++;
+		}
+	}
+	Array.Resize(ref result, count);
+			
+	return result;
+}
